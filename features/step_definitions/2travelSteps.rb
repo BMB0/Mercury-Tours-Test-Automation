b222926fa4047d13580b33baf93e17835618871f
luferchoo <luisfernandogilgareca@gmail.com>
2023-05-31 20:55:04 -0400

    book a costumized flight automated

--- features/step_definitions/2travelSteps.rb
+++ features/step_definitions/2travelSteps.rb
@@ -112,7 +112,8 @@ When('I enter the required fields as show below for my flight') do |table|
   data.each_pair do |key, value|
     case key
     when "Type:"
-      page.choose(value)
+      xpath = '/html/body/div[2]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[2]/td[2]/b/font/input[2]'
+      find(:xpath, xpath).click
     when "Passengers:"
       select value, :from => "passCount"
     when "Departing From:"
@@ -126,7 +127,8 @@ When('I enter the required fields as show below for my flight') do |table|
       select "June", :from => "toMonth"
       select "12", :from => "toDay"
     when "Service Class:"
-      page.choose(value)
+      xpath = '/html/body/div[2]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[9]/td[2]/font/font/input[2]'
+      find(:xpath, xpath).click
     when "Airline:"
       select value, :from => "airline"
     end
